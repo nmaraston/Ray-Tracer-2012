3de5bdcbac0fc40157ad45fd8f250052d3a5aec2
Nick Maraston <nick.maraston@gmail.com>
2013-04-16 14:28:04 -0400

    Converted back to command line project. Removed all xcode/cocoa files.

--- Ray-Tracer/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-#import "ViewportController.h"
-
-@interface AppDelegate : NSObject <NSApplicationDelegate>
-
-@property (assign) IBOutlet NSWindow *window;
-@property (assign) IBOutlet ViewportController *viewportController;
-
-@end
