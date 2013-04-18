92571b94139f4deb023cac1bac8c0cd12befae5a
Nick Maraston <nick.maraston@gmail.com>
2013-04-18 01:59:05 -0400

    Moved headers into seperate 'include' folder.

--- Makefile
+++ Makefile
@@ -4,16 +4,17 @@ BUILD_DIR 	= build
 OBJ_DIR 	= $(BUILD_DIR)/obj
 
 CC			= g++
-CC_FLAGS	= -Wall
+CC_FLAGS	= -Wall 
 
 CPP_FILES 	:= $(wildcard $(SRC_DIR)/*.cpp)
+H_FILES		:= $(wildcard $(INC_DIR)/*.h)
 OBJ_FILES 	:= $(addprefix $(OBJ_DIR)/,$(notdir $(CPP_FILES:.cpp=.o)))
 
 ray-tracer: make_build_dir $(OBJ_FILES)
 	$(CC) $(CC_FLAGS) $(OBJ_FILES) -o $(BUILD_DIR)/$@
 
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
-	$(CC) -c $^ -o $@
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp $(H_FILES)
+	$(CC) $(CC_FLAGS) -c $< -I$(INC_DIR) -o $@
 
 make_build_dir:
 	mkdir -p $(BUILD_DIR)
--- src/main.cpp
+++ src/main.cpp
@@ -6,35 +6,53 @@
 #include "Renderer.h"
 
 /* 
- * Ray-tracing demo.
+ * Ray-tracer demo.
  */
 int main(int argc, char** argv)
 {	
-	Vector3d sphere1_center(-20, 0, -60);
-	Sphere sphere1(sphere1_center, 18);
+	// Create materials.
+	Material turquoise(Spectrum(0.17, 0.68, 0.59));
+	Material yellow(Spectrum(0.68, 0.87, 0.3));
+	Material pink(Spectrum(0.89, 0.47, 0.47));
 
-    Vector3d sphere2_center(20, 0, -60);
-    Sphere sphere2(sphere2_center, 18);
+	// Create scene geometry (only spheres).
+	Vector3d sphere1_center(-20, 20, -60);
+	Sphere sphere1(sphere1_center, 18, &turquoise);
 
-	// Create lights.
-	Vector3d light1_origin(-40, 35, 0);
-	Light light1(light1_origin, 1);
+    Vector3d sphere2_center(20, 20, -60);
+    Sphere sphere2(sphere2_center, 18, &yellow);
 
-	// Set up the scene with a single sphere sitting in the negative region of the z-axis.
+    Vector3d sphere3_center(-20, -20, -60);
+    Sphere sphere3(sphere3_center, 18, &pink);
+
+    Vector3d sphere4_center(20, -20, -60);
+    Sphere sphere4(sphere4_center, 18);
+
+	// Create lights (only point lights).
+	Vector3d light1_origin(-40, 20, 0);
+	Vector3d light2_origin(50, 20, 0);
+	Light light1(light1_origin, 0.4);
+	Light light2(light2_origin, 0.8);
+
+	// Set up the scene with a single sphere sitting in the negative region of 
+	// the z-axis.
 	Scene scene;
     scene.add_surface(&sphere1);
 	scene.add_surface(&sphere2);
+	scene.add_surface(&sphere3);
+	scene.add_surface(&sphere4);
 	scene.add_light(&light1);
+	scene.add_light(&light2);
 
-	// Set up camera on the z-axis directed towards the negative region of the z-axis with a focal
-	// distance of 5.
+	// Set up camera on the z-axis directed towards the negative region of the 
+	// z-axis with a focal distance of 5.
 	Vector3d cam_origin(0, 0, 0);
 	Vector3d cam_direction(0, 0, -1);
 	Vector3d up(0, 1, 0);
-	Camera cam(cam_origin, cam_direction, up, 50);
+	Camera cam(cam_origin, cam_direction, up, 100);
 
 	// Image speifications.
-	ImagePlane imgpl(100, 100, 500, 500);
+	ImagePlane imgpl(200, 150, 600, 450);
 
 	// Set up the renderer.
 	Renderer renderer(&scene, &cam, &imgpl);
